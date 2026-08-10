7a24ffe150decb53ed0d53260b41b4a3b2cac45f
simoneavella-cpu <simone.avella@gmail.com>
2026-08-10 21:31:17 +0200

    chore: remove all private files and rewrite .gitignore definitively

--- script_locali/Spegni_Agenda.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Sto spegnendo l'Agenda...
-:: Trova il PID del processo sulla porta 8080 e lo chiude
-for /f "tokens=5" %%a in ('netstat -aon ^| findstr :8080') do (
-    taskkill /f /pid %%a
-)
-echo Agenda spenta correttamente.
-pause
